03bc3da86516dc3e9737846e164708c836c02a4a
Chaitannyaa <crmg26696@gmail.com>
2023-04-19 14:28:41 +0000

    Add new project with Dockerfile

---
+++ Calculator/README.md
@@ -0,0 +1,18 @@
+
+# calculator Web Application
+
+Type: Individual Mini Project
+
+Time: 1 Week
+
+TechStack: HTML | CSS | JavaScript
+
+Description: 
+I have made this calcutator using the HTML, CSS, and JavaScript. It was an individual mini project.
+
+### Live Link
+[Checkout](https://gentle-dragon-7c373c.netlify.app)
+
+### Calculator UI
+![Screenshot 2022-11-04 193742](https://user-images.githubusercontent.com/76080960/199995189-c477a12d-45db-445e-a4a7-11c8d74f95c3.png)
+
---
+++ Calculator/script.js
@@ -0,0 +1,32 @@
+const num = document.querySelectorAll("#keyboard>div");
+const dis = document.querySelector("#input");
+
+for (i = 0; i < num.length; i++) {
+  num[i].addEventListener("click", cal);
+}
+
+function cal() {
+  let tar = event.target.innerText;
+  let a = dis.innerText;
+  let b = a.slice(0, -1);
+
+  switch (tar) {
+    case "C":
+      dis.innerText = "";
+      break;
+
+    case "=":
+      if (a[0] == "/" || a[0] == "*") {
+        dis.innerText = "Please check the input";
+      } else if (dis.innerText != "") {
+        dis.innerText = eval(dis.innerText);
+      }
+      break;
+    case "Back":
+      dis.innerText = b;
+
+      break;
+    default:
+      dis.innerText += tar;
+  }
+}
