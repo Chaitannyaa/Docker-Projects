d76184c20258d298f36d923be67687956ed72618
Ubuntu <ubuntu@ip-10-0-1-181.ec2.internal>
2023-04-23 14:46:44 +0000

    Fix MySQL connectivity issue

--- Project-Shell_vs_Python_Scripting_Docker-Compose/app.py
+++ Project-Shell_vs_Python_Scripting_Docker-Compose/app.py
@@ -1,27 +1,29 @@
 from flask import Flask, render_template, request
 import mysql.connector
+import time
 
 app = Flask(__name__)
 
-# Connect to the MySQL database
+time.sleep(30)  # wait for 10 seconds
+
+# Connect to the MySQL server
 db = mysql.connector.connect(
-  host="mysql",
+  host="mysql-container",
   user="root",
   password="password",
 )
 
-# Create the database if it doesn't exist
+# Create the 'votes' database if it doesn't exist
 cursor = db.cursor()
 cursor.execute("CREATE DATABASE IF NOT EXISTS votes")
 
-# Connect to the votes database
+# Connect to the 'votes' database
 db = mysql.connector.connect(
-  host="mysql",
+  host="mysql-container",
   user="root",
   password="password",
   database="votes"
 )
-cursor = db.cursor()
 
 # Define the index route to display the voting form
 @app.route('/', methods=['GET', 'POST'])
@@ -29,9 +31,11 @@ def index():
     if request.method == 'POST':
         # Increment the vote count for the selected language
         language = request.form['language']
+        cursor = db.cursor()
         cursor.execute(f"UPDATE vote_counts SET count = count + 1 WHERE language = '{language}'")
         db.commit()
     # Retrieve the vote counts from the database
+    cursor = db.cursor()
     cursor.execute("SELECT language, count FROM vote_counts")
     rows = cursor.fetchall()
     vote_counts = {}
@@ -42,9 +46,11 @@ def index():
 
 if __name__ == '__main__':
     # Create the vote_counts table if it doesn't exist
+    cursor = db.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS vote_counts (language VARCHAR(255), count INT)")
     # Initialize the vote counts to zero if they don't exist
     cursor.execute("INSERT IGNORE INTO vote_counts (language, count) VALUES ('Python', 0), ('Shell', 0)")
     db.commit()
     # Start the web server
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
+
